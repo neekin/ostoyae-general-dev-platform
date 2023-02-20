2c3d241ac81ad5310721d40634c0f2ff5b582864
NeeKin <neekin@192.168.2.65>
2023-02-21 06:11:16 +0800

    规范模块

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShopModule } from '@ostoyae/shop';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ShopModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.dev.sqlite3',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ShopModule,
+  ],
   controllers: [],
   providers: [],
 })
